1be762339f1fb65a97d13d6b99ce3bee7513509c
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2023-08-15 08:53:36 +0200

    Update code_pompes.ino

--- code_pompes/code_pompes.ino
+++ code_pompes/code_pompes.ino
@@ -3,19 +3,16 @@
 #include <NewPing.h>
 
 // Tableau broches
-const int broches[10] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
-/* Pour note tableau
-pompe1Pin = 2;
-pompe2Pin = 3;
-boutonPoussoirPompe1 = 4;
-boutonPoussoirPompe2 = 5;
-interSelecteurMode = 6;
-interSelecteurModeAdj = 7;
-boutonPoussoirAdjPlus = 8;
-boutonPoussoirAdjMoins = 9;
-trigger = 10;
-echo = 11;
-*/
+int pompe1Pin = 2;
+int pompe2Pin = 3;
+int boutonPoussoirPompe1 = 4;
+int boutonPoussoirPompe2 = 5;
+int interSelecteurMode = 6;
+int interSelecteurModeAdj = 7;
+int boutonPoussoirAdjPlus = 8;
+int boutonPoussoirAdjMoins = 9;
+int trigger = 10;
+int echo = 11;
 
 // Variables
 const int seuilPompe1 = 50;
@@ -33,24 +30,24 @@ boolean bpPompe2 = false;
 boolean bpAdjPlus = false;
 boolean bpAdjMoins = false;
 boolean modeManuel = false;
-boolean modeAuto = false;
+boolean modeAuto = true;
 boolean modeAdj = false;
 
 // Modules externes
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-NewPing sonar(broches[8], broches[9], max_distance);
+NewPing sonar(trigger, echo, max_distance);
 
 void setup() {
-  pinMode(broches[0], OUTPUT);
-  pinMode(broches[1], OUTPUT);
-  pinMode(broches[2], INPUT);
-  pinMode(broches[3], INPUT);
-  pinMode(broches[4], INPUT);
-  pinMode(broches[5], INPUT);
-  pinMode(broches[6], INPUT);
-  pinMode(broches[7], INPUT);
-  pinMode(broches[8], OUTPUT);
-  pinMode(broches[9], INPUT);
+  pinMode(pompe1Pin, OUTPUT);
+  pinMode(pompe2Pin, OUTPUT);
+  pinMode(boutonPoussoirPompe1, INPUT);
+  pinMode(boutonPoussoirPompe2, INPUT);
+  pinMode(interSelecteurMode, INPUT);
+  pinMode(interSelecteurModeAdj, INPUT);
+  pinMode(boutonPoussoirAdjPlus, INPUT);
+  pinMode(boutonPoussoirAdjMoins, INPUT);
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
   // Les broches sont LOW au démarrage
@@ -61,28 +58,62 @@ void setup() {
   lcd.backlight();
 }
 
+void debug() {
+  Serial.println("modeManuel");
+  Serial.println(modeManuel);
+  Serial.println("------");
+  delay(1000);
+  Serial.println("modeAuto");
+  Serial.println(modeAuto);
+  Serial.println("------");
+  delay(1000);
+  Serial.println("modeAdj");
+  Serial.println(modeAdj);
+  Serial.println("------");
+  delay(1000);
+  Serial.println("selecteurMode");
+  Serial.println(selecteurMode);
+  Serial.println("------");
+  delay(1000);
+  Serial.println("selecteurModeAdj");
+  Serial.println(selecteurModeAdj);
+  Serial.println("------");
+  delay(1000);
+  Serial.println("etatPompe1Pin");
+  Serial.println(etatPompe1Pin);
+  Serial.println("------");
+  delay(1000);
+  Serial.println("etatPompe2Pin");
+  Serial.println(etatPompe2Pin);
+  Serial.println("------");
+  delay(1000);
+  Serial.println("bpPompe1");
+  Serial.println(bpPompe1);
+  Serial.println("------");
+  delay(1000);
+  Serial.println("bpPompe2");
+  Serial.println(bpPompe2);
+  Serial.println("------");
+  delay(1000);
+  Serial.println("bpAdjPlus");
+  Serial.println(bpAdjPlus);
+  Serial.println("------");
+  delay(1000);
+  Serial.println("bpAdjMoins");
+  Serial.println(bpAdjMoins);
+  Serial.println("------");
+  delay(1000);
+}
+
 void loop() {
 
   distance = sonar.ping_cm();
 
-  Serial.println("modeManuel");
-  Serial.println(modeManuel);
-  Serial.println("------");
-  delay(100);
+  debug();
 
-  Serial.println("modeAuto");
-  Serial.println(modeAuto);
-  Serial.println("------");
-  delay(100);
-
-  Serial.println("modeAdj");
-  Serial.println(modeAdj);
-  Serial.println("------");
-  delay(100);
-
-  if (digitalRead(broches[4]) == LOW) {  // Selecteur Mode
-    modeManuel = false;                  // Mode manuel off
-    modeAuto = true;                     // Mode auto on
+  if (digitalRead(interSelecteurMode) == LOW) {  // Selecteur Mode
+    modeManuel = false;                          // Mode manuel off
+    modeAuto = true;                             // Mode auto on
     delay(250);
   } else {
     modeManuel = true;  // Mode manuel on
@@ -90,7 +121,7 @@ void loop() {
     delay(250);
   }
 
-  if (digitalRead(broches[5]) == LOW) {  // Selecteur modeAdj
+  if (digitalRead(interSelecteurModeAdj) == LOW) {  // Selecteur modeAdj
     modeAdj = false;
     delay(250);
   } else {
@@ -100,19 +131,19 @@ void loop() {
 
 
   if (modeManuel) {
-    if (broches[2] == LOW) {          // boutonPoussoirPompe1
-      digitalWrite(broches[0], LOW);  // pompe1Pin
+    if (boutonPoussoirPompe1 == LOW) {  // boutonPoussoirPompe1
+      digitalWrite(pompe1Pin, LOW);     // pompe1Pin
       etatPompe1Pin = false;
     } else {
-      digitalWrite(broches[0], HIGH);  // pompe1Pin
+      digitalWrite(pompe1Pin, HIGH);  // pompe1Pin
       etatPompe1Pin = true;
     }
 
-    if (broches[3] == LOW) {          // boutonPoussoirPompe2
-      digitalWrite(broches[1], LOW);  // pompe2Pin
+    if (boutonPoussoirPompe2 == LOW) {  // boutonPoussoirPompe2
+      digitalWrite(pompe2Pin, LOW);     // pompe2Pin
       etatPompe2Pin = false;
     } else {
-      digitalWrite(broches[1], HIGH);  // pompe2Pin
+      digitalWrite(pompe2Pin, HIGH);  // pompe2Pin
       etatPompe2Pin = true;
     }
   }
