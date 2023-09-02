70932e0b432118d600e1279364f88376ed85b072
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2023-09-02 02:27:20 +0200

    Update code_pompes.ino

--- code_pompes/code_pompes.ino
+++ code_pompes/code_pompes.ino
@@ -3,7 +3,6 @@
 #include <LiquidCrystal_I2C.h>
 #include <NewPing.h>
 
-// Broches
 const int pompe1Pin = 2;
 const int pompe2Pin = 3;
 const int boutonPoussoirPompe1 = 4;
@@ -15,9 +14,8 @@ const int boutonPoussoirAdjPlus = 9;
 const int TRIGGER_PIN = 10;
 const int ECHO_PIN = 11;
 
-// Variables
-const int shP1address = 0;  // L'adresse de l'EEPROM où vous voulez stocker la valeur
-const int shP2address = 1;  // L'adresse de l'EEPROM où vous voulez stocker la valeur
+const int shP1address = 0;
+const int shP2address = 1;
 
 int seuilHautP1 = 0;
 const int seuilBasP1 = 25;
@@ -31,7 +29,6 @@ String modeStr = "";
 long duration;
 int distance;
 
-// États
 int selecteurMode = 0;
 int selecteurModeAdj = 0;
 int etatPompe1Pin = 0;
@@ -44,7 +41,6 @@ int modeManuel = 0;
 int modeAuto = 1;
 int modeAdj = 0;
 
-// Modules externes
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 LiquidCrystal_I2C lcd1(0x26, 16, 2);
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
@@ -72,7 +68,7 @@ void setup() {
   digitalWrite(pompe1Pin, LOW);
   digitalWrite(pompe2Pin, LOW);
 
-  int shP1readValue = EEPROM.read(shP1address);  // Lecture de la valeur depuis l'EEPROM
+  int shP1readValue = EEPROM.read(shP1address);
   seuilHautP1 = shP1readValue;
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -82,7 +78,7 @@ void setup() {
 
   delay(2000);
 
-  int shP2readValue = EEPROM.read(shP2address);  // Lecture de la valeur depuis l'EEPROM
+  int shP2readValue = EEPROM.read(shP2address);
   seuilHautP2 = shP2readValue;
   lcd.setCursor(0, 1);
   lcd.print("Read shP2: ");
@@ -90,7 +86,7 @@ void setup() {
   lcd.print(shP2readValue);
 
   delay(5000);
-  
+
   lcd.clear();
 }
 
@@ -112,18 +108,18 @@ void loop() {
     lcd.print(distance);
   }
 
-  // Effacez le trigPin en le définissant sur LOW :
+
   digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(5);
 
-  // Déclenchez le capteur en réglant le trigPin haut pendant 10 microsecondes :
+
   digitalWrite(TRIGGER_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIGGER_PIN, LOW);
 
-  // Lit l'echoPin, pulseIn() renvoie la durée (longueur de l'impulsion) en microsecondes :
+
   duration = pulseIn(ECHO_PIN, HIGH);
-  // Calcule la distance :
+  
   distance = duration * 0.034 / 2;
 
   selecteurMode = digitalRead(interSelecteurMode);
@@ -143,44 +139,44 @@ void loop() {
     modeAdj = 1;
   }
 
-  if ((modeManuel) && (!modeAdj)) {                // mode manu et pas mode d'ajustement
-    bpPompe1 = digitalRead(boutonPoussoirPompe1);  //Lecture poussoir pompe 1
-    bpPompe2 = digitalRead(boutonPoussoirPompe2);  //Lecture poussoir pompe 2
+  if ((modeManuel) && (!modeAdj)) {
+    bpPompe1 = digitalRead(boutonPoussoirPompe1);
+    bpPompe2 = digitalRead(boutonPoussoirPompe2);
 
     lcd.setCursor(0, 2);
     lcd.print("            ");
 
     modeStr = "Mode: Manuel";
 
-    if ((bpPompe1) == 0) {            // boutonPoussoirPompe1 pas actionné
-      digitalWrite(pompe1Pin, HIGH);  // Pompe1 sur OFF pompe1Pin
+    if ((bpPompe1) == 0) {
+      digitalWrite(pompe1Pin, HIGH);
       etatPompe1Str = "P1: OFF";
       etatPompe1Pin = 0;
-    } else if ((bpPompe1) == 1) {    // boutonPoussoirPompe1 actionné
-      digitalWrite(pompe1Pin, LOW);  // Pompe1 sur ON pompe1Pin
+    } else if ((bpPompe1) == 1) {
+      digitalWrite(pompe1Pin, LOW);
       etatPompe1Str = "P1: ON ";
       etatPompe1Pin = 1;
     }
 
-    if ((bpPompe2) == 0) {            // boutonPoussoirPompe1 pas actionné
-      digitalWrite(pompe2Pin, HIGH);  // Pompe1 sur OFF pompe1Pin
+    if ((bpPompe2) == 0) {
+      digitalWrite(pompe2Pin, HIGH);
       etatPompe2Str = "P2: OFF";
       etatPompe2Pin = 0;
-    } else if ((bpPompe2) == 1) {    // boutonPoussoirPompe1 actionné
-      digitalWrite(pompe2Pin, LOW);  // Pompe1 sur ON pompe1Pin
+    } else if ((bpPompe2) == 1) {
+      digitalWrite(pompe2Pin, LOW);
       etatPompe2Str = "P2: ON ";
       etatPompe2Pin = 1;
     }
   }
 
-  if ((modeManuel) && (modeAdj)) {                     // mode manu et mode d'ajustement
-    bpAdjPlus = digitalRead(boutonPoussoirAdjPlus);    //Lecture poussoir adj plus
-    bpAdjMoins = digitalRead(boutonPoussoirAdjMoins);  //Lecture poussoir adj moins
-    bpPompe1 = digitalRead(boutonPoussoirPompe1);      //Lecture poussoir pompe 1
-    bpPompe2 = digitalRead(boutonPoussoirPompe2);      //Lecture poussoir pompe 2
-    digitalWrite(pompe1Pin, LOW);                      // Pompe1 sur OFF pompe1Pin
+  if ((modeManuel) && (modeAdj)) {
+    bpAdjPlus = digitalRead(boutonPoussoirAdjPlus);
+    bpAdjMoins = digitalRead(boutonPoussoirAdjMoins);
+    bpPompe1 = digitalRead(boutonPoussoirPompe1);
+    bpPompe2 = digitalRead(boutonPoussoirPompe2);
+    digitalWrite(pompe1Pin, LOW);
     etatPompe1Pin = 0;
-    digitalWrite(pompe2Pin, LOW);  // pompe2 sur OFF pompe2Pin
+    digitalWrite(pompe2Pin, LOW);
     etatPompe2Pin = 0;
 
     modeStr = "Mode: Adjust";
@@ -252,7 +248,7 @@ void loop() {
     lcd.print("Write shP2...");
   }
 
-  if ((modeAuto) && (!modeManuel)) {  // mode auto et pas mode manu
+  if ((modeAuto) && (!modeManuel)) {
 
     lcd.setCursor(0, 2);
     lcd.print("            ");
@@ -260,21 +256,21 @@ void loop() {
     modeStr = "Mode: Auto  ";
 
     if (distance <= seuilHautP1) {
-      digitalWrite(pompe1Pin, HIGH);  // Pompe1 sur ON pompe1Pin
+      digitalWrite(pompe1Pin, HIGH);
       etatPompe1Str = "P1: ON ";
       etatPompe1Pin = 1;
     } else if (distance >= seuilBasP1) {
-      digitalWrite(pompe1Pin, LOW);  // Pompe1 sur ON pompe1Pin
+      digitalWrite(pompe1Pin, LOW);
       etatPompe1Str = "P1: OFF";
       etatPompe1Pin = 0;
     }
 
     if (distance <= seuilHautP2) {
-      digitalWrite(pompe2Pin, HIGH);  // Pompe1 sur ON pompe1Pin
+      digitalWrite(pompe2Pin, HIGH);
       etatPompe2Str = "P2: ON ";
       etatPompe2Pin = 1;
     } else if (distance >= seuilBasP2) {
-      digitalWrite(pompe2Pin, LOW);  // Pompe1 sur ON pompe1Pin
+      digitalWrite(pompe2Pin, LOW);
       etatPompe2Str = "P2: OFF";
       etatPompe2Pin = 0;
     }
