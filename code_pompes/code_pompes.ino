cadfaacb96ce13198c66bf951b10938c4eae0e4c
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2023-09-02 02:31:22 +0200

    Update code_pompes.ino

--- code_pompes/code_pompes.ino
+++ code_pompes/code_pompes.ino
@@ -1,8 +1,9 @@
-#include <EEPROM.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-#include <NewPing.h>
+#include <EEPROM.h>               // Bibliothèque pour l'accès à l'EEPROM
+#include <Wire.h>                 // Bibliothèque pour la communication I2C
+#include <LiquidCrystal_I2C.h>    // Bibliothèque pour les écrans LCD I2C
+#include <NewPing.h>              // Bibliothèque pour le capteur ultrasonique
 
+// Déclaration des broches matérielles
 const int pompe1Pin = 2;
 const int pompe2Pin = 3;
 const int boutonPoussoirPompe1 = 4;
@@ -14,14 +15,17 @@ const int boutonPoussoirAdjPlus = 9;
 const int TRIGGER_PIN = 10;
 const int ECHO_PIN = 11;
 
+// Adresses EEPROM pour le stockage de données
 const int shP1address = 0;
 const int shP2address = 1;
 
+// Variables pour les seuils de déclenchement des pompes
 int seuilHautP1 = 0;
 const int seuilBasP1 = 25;
 int seuilHautP2 = 0;
 const int seuilBasP2 = 23;
 
+// Constante pour la distance maximale du capteur ultrasonique
 const int MAX_DISTANCE = 40;
 String etatPompe1Str = "";
 String etatPompe2Str = "";
@@ -29,6 +33,7 @@ String modeStr = "";
 long duration;
 int distance;
 
+// Variables de contrôle et d'état
 int selecteurMode = 0;
 int selecteurModeAdj = 0;
 int etatPompe1Pin = 0;
@@ -41,21 +46,24 @@ int modeManuel = 0;
 int modeAuto = 1;
 int modeAdj = 0;
 
+// Initialisation des modules externes
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 LiquidCrystal_I2C lcd1(0x26, 16, 2);
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 void setup() {
+  // Initialisation de la communication série
   Serial.begin(9600);
 
+  // Initialisation des écrans LCD
   lcd.init();
   lcd.backlight();
   lcd1.init();
   lcd1.backlight();
 
+  // Configuration des broches en entrée ou en sortie
   pinMode(pompe1Pin, OUTPUT);
   pinMode(pompe2Pin, OUTPUT);
-
   pinMode(boutonPoussoirPompe1, INPUT);
   pinMode(boutonPoussoirPompe2, INPUT);
   pinMode(interSelecteurMode, INPUT);
@@ -65,6 +73,7 @@ void setup() {
   pinMode(ECHO_PIN, INPUT);
   pinMode(TRIGGER_PIN, OUTPUT);
 
+  // Lecture des seuils à partir de l'EEPROM et affichage
   int shP1readValue = EEPROM.read(shP1address);
   seuilHautP1 = shP1readValue;
   lcd.clear();
@@ -88,8 +97,10 @@ void setup() {
 }
 
 void loop() {
-
+  // Attente courte pour éviter de saturer la boucle
   delay(2);
+
+  // Affichage des informations sur l'écran LCD
   lcd.setCursor(0, 0);
   lcd.print(modeStr);
   lcd.setCursor(0, 1);
@@ -105,23 +116,20 @@ void loop() {
     lcd.print(distance);
   }
 
-
+  // Mesure de la distance avec le capteur ultrasonique
   digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(5);
-
-
   digitalWrite(TRIGGER_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIGGER_PIN, LOW);
-
-
   duration = pulseIn(ECHO_PIN, HIGH);
-  
   distance = duration * 0.034 / 2;
 
+  // Lecture des états des sélecteurs de mode
   selecteurMode = digitalRead(interSelecteurMode);
   selecteurModeAdj = digitalRead(interSelecteurModeAdj);
 
+  // Logique de sélection du mode (manuel ou automatique)
   if (selecteurMode == LOW) {
     modeManuel = 0;
     modeAuto = 1;
@@ -130,12 +138,14 @@ void loop() {
     modeAuto = 0;
   }
 
+  // Logique de sélection du mode d'ajustement
   if (selecteurModeAdj == LOW) {
     modeAdj = 0;
   } else if (selecteurModeAdj == HIGH) {
     modeAdj = 1;
   }
 
+  // Logique pour le mode manuel sans ajustement
   if ((modeManuel) && (!modeAdj)) {
     bpPompe1 = digitalRead(boutonPoussoirPompe1);
     bpPompe2 = digitalRead(boutonPoussoirPompe2);
@@ -145,27 +155,28 @@ void loop() {
 
     modeStr = "Mode: Manuel";
 
-    if ((bpPompe1) == 0) {
+    if (bpPompe1 == 0) {
       digitalWrite(pompe1Pin, HIGH);
       etatPompe1Str = "P1: OFF";
       etatPompe1Pin = 0;
-    } else if ((bpPompe1) == 1) {
+    } else if (bpPompe1 == 1) {
       digitalWrite(pompe1Pin, LOW);
       etatPompe1Str = "P1: ON ";
       etatPompe1Pin = 1;
     }
 
-    if ((bpPompe2) == 0) {
+    if (bpPompe2 == 0) {
       digitalWrite(pompe2Pin, HIGH);
       etatPompe2Str = "P2: OFF";
       etatPompe2Pin = 0;
-    } else if ((bpPompe2) == 1) {
+    } else if (bpPompe2 == 1) {
       digitalWrite(pompe2Pin, LOW);
       etatPompe2Str = "P2: ON ";
       etatPompe2Pin = 1;
     }
   }
 
+  // Logique pour le mode manuel avec ajustement
   if ((modeManuel) && (modeAdj)) {
     bpAdjPlus = digitalRead(boutonPoussoirAdjPlus);
     bpAdjMoins = digitalRead(boutonPoussoirAdjMoins);
@@ -178,6 +189,7 @@ void loop() {
 
     modeStr = "Mode: Adjust";
 
+    // Logique pour ajuster le seuil de la pompe 1
     if ((bpAdjPlus) && (bpPompe1)) {
       lcd.setCursor(0, 2);
       lcd.print("Seuil P1: ");
@@ -205,6 +217,8 @@ void loop() {
         seuilHautP1 = seuilHautP1 - 1;
       }
     }
+
+    // Écriture du seuil ajusté dans l'EEPROM
     EEPROM.write(shP1address, seuilHautP1);
     Serial.println("Write shP1:");
     lcd.clear();
@@ -212,6 +226,7 @@ void loop() {
     lcd.print("Write shP1...");
   }
 
+  // Logique pour ajuster le seuil de la pompe 2
   if ((bpAdjPlus) && (bpPompe2)) {
     lcd.setCursor(0, 2);
     lcd.print("Seuil P2: ");
@@ -238,6 +253,8 @@ void loop() {
     if (seuilHautP2 > 0) {
       seuilHautP2 = seuilHautP2 - 1;
     }
+    
+    // Écriture du seuil ajusté dans l'EEPROM
     EEPROM.write(shP2address, seuilHautP2);
     Serial.println("Write shP2");
     lcd.clear();
@@ -245,13 +262,13 @@ void loop() {
     lcd.print("Write shP2...");
   }
 
+  // Logique pour le mode automatique
   if ((modeAuto) && (!modeManuel)) {
-
     lcd.setCursor(0, 2);
     lcd.print("            ");
-
     modeStr = "Mode: Auto  ";
 
+    // Logique pour activer/désactiver la pompe 1 en fonction de la distance
     if (distance <= seuilHautP1) {
       digitalWrite(pompe1Pin, HIGH);
       etatPompe1Str = "P1: ON ";
@@ -262,6 +279,7 @@ void loop() {
       etatPompe1Pin = 0;
     }
 
+    // Logique pour activer/désactiver la pompe 2 en fonction de la distance
     if (distance <= seuilHautP2) {
       digitalWrite(pompe2Pin, HIGH);
       etatPompe2Str = "P2: ON ";
