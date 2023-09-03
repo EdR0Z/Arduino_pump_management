47ecc495ea74f19a69ed7846541fc46409f97dd9
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2023-09-03 18:02:17 +0200

    Update code_pompes.ino

--- code_pompes/code_pompes.ino
+++ code_pompes/code_pompes.ino
@@ -1,8 +1,9 @@
-#include <EEPROM.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-#include <NewPing.h>
+#include <EEPROM.h>               // Bibliothèque pour l'accès à l'EEPROM
+#include <Wire.h>                 // Bibliothèque pour la communication I2C
+#include <LiquidCrystal_I2C.h>    // Bibliothèque pour les écrans LCD I2C
+#include <NewPing.h>              // Bibliothèque pour le capteur ultrasonique
 
+// Déclaration des broches matérielles
 const int pompe1Pin = 2;
 const int pompe2Pin = 3;
 const int boutonPoussoirPompe1 = 4;
@@ -14,21 +15,25 @@ const int boutonPoussoirAdjPlus = 9;
 const int TRIGGER_PIN = 10;
 const int ECHO_PIN = 11;
 
+// Adresses EEPROM pour le stockage de données
 const int shP1address = 0;
 const int shP2address = 1;
 
+// Variables pour les seuils de déclenchement des pompes
 int seuilHautP1 = 0;
 const int seuilBasP1 = 25;
 int seuilHautP2 = 0;
 const int seuilBasP2 = 23;
 
-const int MAX_DISTANCE = 400; // Updated the MAX_DISTANCE value to 400 cm
+// Constante pour la distance maximale du capteur ultrasonique
+const int MAX_DISTANCE = 40;
 String etatPompe1Str = "";
 String etatPompe2Str = "";
 String modeStr = "";
 long duration;
 int distance;
 
+// Variables de contrôle et d'état
 int selecteurMode = 0;
 int selecteurModeAdj = 0;
 int etatPompe1Pin = 0;
@@ -41,26 +46,24 @@ int modeManuel = 0;
 int modeAuto = 1;
 int modeAdj = 0;
 
-LiquidCrystal_I2C lcd(0x27, 20, 4); // Configuration for a 20x4 I2C LCD
+// Initialisation des modules externes
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd1(0x26, 16, 2);
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
-// Constants for error handling
-const int NO_ECHO_ERROR = -1;
-const int MAX_DISTANCE_ERROR = -2;
-
-// Variables for debouncing
-unsigned long lastDebounceTime = 0;
-unsigned long debounceDelay = 50;
-
 void setup() {
+  // Initialisation de la communication série
   Serial.begin(9600);
 
+  // Initialisation des écrans LCD
   lcd.init();
   lcd.backlight();
+  lcd1.init();
+  lcd1.backlight();
 
+  // Configuration des broches en entrée ou en sortie
   pinMode(pompe1Pin, OUTPUT);
   pinMode(pompe2Pin, OUTPUT);
-
   pinMode(boutonPoussoirPompe1, INPUT);
   pinMode(boutonPoussoirPompe2, INPUT);
   pinMode(interSelecteurMode, INPUT);
@@ -70,6 +73,7 @@ void setup() {
   pinMode(ECHO_PIN, INPUT);
   pinMode(TRIGGER_PIN, OUTPUT);
 
+  // Lecture des seuils à partir de l'EEPROM et affichage
   int shP1readValue = EEPROM.read(shP1address);
   seuilHautP1 = shP1readValue;
   lcd.clear();
@@ -93,36 +97,39 @@ void setup() {
 }
 
 void loop() {
+  // Attente courte pour éviter de saturer la boucle
   delay(2);
+
+  // Affichage des informations sur l'écran LCD
   lcd.setCursor(0, 0);
   lcd.print(modeStr);
   lcd.setCursor(0, 1);
   lcd.print(etatPompe1Str + " " + etatPompe2Str);
   lcd.setCursor(0, 3);
   lcd.print("Distance: ");
-
-  // Measure distance with error handling
-  int distanceError = mesurerDistance();
-  if (distanceError == NO_ECHO_ERROR) {
-    lcd.setCursor(11, 3);
-    lcd.print("Err: No Echo");
-  } else if (distanceError == MAX_DISTANCE_ERROR) {
-    lcd.setCursor(11, 3);
-    lcd.print("Err: Max Dist");
+  if (distance < 10) {
+    lcd.setCursor(10, 3);
+    lcd.print(0);
+    lcd.print(distance);
   } else {
-    if (distance < 10) {
-      lcd.setCursor(11, 3);
-      lcd.print(0);
-      lcd.print(distance);
-    } else {
-      lcd.setCursor(11, 3);
-      lcd.print(distance);
-    }
+    lcd.setCursor(10, 3);
+    lcd.print(distance);
   }
 
+  // Mesure de la distance avec le capteur ultrasonique
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(5);
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = duration * 0.034 / 2;
+
+  // Lecture des états des sélecteurs de mode
   selecteurMode = digitalRead(interSelecteurMode);
   selecteurModeAdj = digitalRead(interSelecteurModeAdj);
 
+  // Logique de sélection du mode (manuel ou automatique)
   if (selecteurMode == LOW) {
     modeManuel = 0;
     modeAuto = 1;
@@ -131,73 +138,156 @@ void loop() {
     modeAuto = 0;
   }
 
+  // Logique de sélection du mode d'ajustement
   if (selecteurModeAdj == LOW) {
     modeAdj = 0;
   } else if (selecteurModeAdj == HIGH) {
     modeAdj = 1;
   }
 
+  // Logique pour le mode manuel sans ajustement
   if ((modeManuel) && (!modeAdj)) {
-    mettreAJourPompesManuel();
-  }
+    bpPompe1 = digitalRead(boutonPoussoirPompe1);
+    bpPompe2 = digitalRead(boutonPoussoirPompe2);
 
-  if ((modeManuel) && (modeAdj)) {
-    ajusterSeuils();
-  }
+    lcd.setCursor(0, 2);
+    lcd.print("            ");
 
-  if ((modeAuto) && (!modeManuel)) {
-    // Logic for automatic mode here
-  }
-}
+    modeStr = "Mode: Manuel";
 
-int mesurerDistance() {
-  // Measure distance with error handling
-  unsigned int uS = sonar.ping();
-  if (uS == NO_ECHO || uS == MAX_DISTANCE) {
-    return uS; // Return the error
-  }
-  return uS / US_ROUNDTRIP_CM;
-}
-
-void mettreAJourPompesManuel() {
-  int bpPompe1State = digitalRead(boutonPoussoirPompe1);
-  int bpPompe2State = digitalRead(boutonPoussoirPompe2);
-
-  if (bpPompe1State != bpPompe1) {
-    lastDebounceTime = millis();
-  }
-
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (bpPompe1State == LOW) {
+    if (bpPompe1 == 0) {
       digitalWrite(pompe1Pin, HIGH);
       etatPompe1Str = "P1: OFF";
       etatPompe1Pin = 0;
-    } else {
+    } else if (bpPompe1 == 1) {
       digitalWrite(pompe1Pin, LOW);
       etatPompe1Str = "P1: ON ";
       etatPompe1Pin = 1;
     }
-    bpPompe1 = bpPompe1State;
-  }
 
-  if (bpPompe2State != bpPompe2) {
-    lastDebounceTime = millis();
-  }
-
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (bpPompe2State == LOW) {
+    if (bpPompe2 == 0) {
       digitalWrite(pompe2Pin, HIGH);
       etatPompe2Str = "P2: OFF";
       etatPompe2Pin = 0;
-    } else {
+    } else if (bpPompe2 == 1) {
       digitalWrite(pompe2Pin, LOW);
       etatPompe2Str = "P2: ON ";
       etatPompe2Pin = 1;
     }
-    bpPompe2 = bpPompe2State;
   }
-}
 
-void ajusterSeuils() {
-  // Logic for adjusting thresholds here
+  // Logique pour le mode manuel avec ajustement
+  if ((modeManuel) && (modeAdj)) {
+    bpAdjPlus = digitalRead(boutonPoussoirAdjPlus);
+    bpAdjMoins = digitalRead(boutonPoussoirAdjMoins);
+    bpPompe1 = digitalRead(boutonPoussoirPompe1);
+    bpPompe2 = digitalRead(boutonPoussoirPompe2);
+    digitalWrite(pompe1Pin, LOW);
+    etatPompe1Pin = 0;
+    digitalWrite(pompe2Pin, LOW);
+    etatPompe2Pin = 0;
+
+    modeStr = "Mode: Adjust";
+
+    // Logique pour ajuster le seuil de la pompe 1
+    if ((bpAdjPlus) && (bpPompe1)) {
+      lcd.setCursor(0, 2);
+      lcd.print("Seuil P1: ");
+      lcd.setCursor(10, 2);
+      if (seuilHautP1 < 10) {
+        lcd.print(0);
+        lcd.print(seuilHautP1);
+      } else {
+        lcd.print(seuilHautP1);
+      }
+      if (seuilHautP1 > 0) {
+        seuilHautP1 = seuilHautP1 + 1;
+      }
+    } else if ((bpAdjMoins) && (bpPompe1)) {
+      lcd.setCursor(0, 2);
+      lcd.print("Seuil P1: ");
+      lcd.setCursor(10, 2);
+      if (seuilHautP1 < 10) {
+        lcd.print(0);
+        lcd.print(seuilHautP1);
+      } else {
+        lcd.print(seuilHautP1);
+      }
+      if (seuilHautP1 > 0) {
+        seuilHautP1 = seuilHautP1 - 1;
+      }
+    }
+
+    // Écriture du seuil ajusté dans l'EEPROM
+    EEPROM.write(shP1address, seuilHautP1);
+    Serial.println("Write shP1:");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Write shP1...");
+  }
+
+  // Logique pour ajuster le seuil de la pompe 2
+  if ((bpAdjPlus) && (bpPompe2)) {
+    lcd.setCursor(0, 2);
+    lcd.print("Seuil P2: ");
+    lcd.setCursor(10, 2);
+    if (seuilHautP2 < 10) {
+      lcd.print(0);
+      lcd.print(seuilHautP2);
+    } else {
+      lcd.print(seuilHautP2);
+    }
+    if (seuilHautP2 > 0) {
+      seuilHautP2 = seuilHautP2 + 1;
+    }
+  } else if ((bpAdjMoins) && (bpPompe2)) {
+    lcd.setCursor(0, 2);
+    lcd.print("Seuil P2: ");
+    lcd.setCursor(10, 2);
+    if (seuilHautP2 < 10) {
+      lcd.print(0);
+      lcd.print(seuilHautP2);
+    } else {
+      lcd.print(seuilHautP2);
+    }
+    if (seuilHautP2 > 0) {
+      seuilHautP2 = seuilHautP2 - 1;
+    }
+    
+    // Écriture du seuil ajusté dans l'EEPROM
+    EEPROM.write(shP2address, seuilHautP2);
+    Serial.println("Write shP2");
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print("Write shP2...");
+  }
+
+  // Logique pour le mode automatique
+  if ((modeAuto) && (!modeManuel)) {
+    lcd.setCursor(0, 2);
+    lcd.print("            ");
+    modeStr = "Mode: Auto  ";
+
+    // Logique pour activer/désactiver la pompe 1 en fonction de la distance
+    if (distance <= seuilHautP1) {
+      digitalWrite(pompe1Pin, HIGH);
+      etatPompe1Str = "P1: ON ";
+      etatPompe1Pin = 1;
+    } else if (distance >= seuilBasP1) {
+      digitalWrite(pompe1Pin, LOW);
+      etatPompe1Str = "P1: OFF";
+      etatPompe1Pin = 0;
+    }
+
+    // Logique pour activer/désactiver la pompe 2 en fonction de la distance
+    if (distance <= seuilHautP2) {
+      digitalWrite(pompe2Pin, HIGH);
+      etatPompe2Str = "P2: ON ";
+      etatPompe2Pin = 1;
+    } else if (distance >= seuilBasP2) {
+      digitalWrite(pompe2Pin, LOW);
+      etatPompe2Str = "P2: OFF";
+      etatPompe2Pin = 0;
+    }
+  }
 }
